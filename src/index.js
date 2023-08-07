542bc3d6a60078e12c544263436f59a575871f23
Shabab Qaisar <sqaisar@pypestream.com>
2023-08-07 14:11:05 +0500

    Added release code

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
-// import * as core from "@actions/core";
-// import * as github from "@actions/github";
-// import * as semver from "semver";
+/*eslint no-undef: "off"*/
+/*eslint no-trailing-spaces: "off"*/
 
 const core   = require("@actions/core");
 const github = require("@actions/github");
-// const semver = require("semver");
+const semver = require("semver");
 const shelljs = require("shelljs");
 
 const owner = github.context.payload.repository.owner.login;
@@ -63,7 +62,7 @@ async function action() {
     const releaseBranch = core.getInput("release-branch");
 
     const activeBranch = github.context.ref.replace(/refs\/heads\//, "");
-    let branchInfo;
+    let branchInfo, releaseType;
 
 
     core.info(`load the history of activity-branch ${ activeBranch } from context ref ${ github.context.ref }`);
@@ -74,38 +73,120 @@ async function action() {
     }
 
     // the sha for tagging
-    const sha        = branchInfo.object.sha;
     const branchName = branchInfo.ref.split("/").pop();
     const currentTagInBranch = await getCurrentTagInBranch();
 
     core.info(`active branch name is ${ branchName }`);
 
-    if(branchName === mainBranch){
-        core.info(`creating tag for pre-release`);
-    } else if (branchName === releaseBranch){
-        core.info(`creating tag for release`);
-    } else {
+    if(![mainBranch, releaseBranch].includes(branchName)){
         throw new Error(`error: this branch ${activeBranch} is not set for release/pre-release`);
+    }
+
+    releaseType = branchName === mainBranch ? "pre-release" : "full-release";
+
+    const tags = await octokit.rest.repos
+    .listTags({
+      owner,
+      repo,
+    })
+    // toss out metadata other than the name of the git tags themselves
+    .then((d) => {
+      return d.data.map((item) => {
+        return item.name;
+      });
+    });
+
+    core.info('git tags from github', tags);
+
+    // get the latest git tag version, INCLUDING pre-releases
+    const latestTagWithPreReleases = semver.maxSatisfying(tags, '*', {
+        includePrerelease: true,
+    });
+
+    // get the latest git tag version, EXCLUDING pre-releases
+    const latestTagWithoutPreReleases = semver.maxSatisfying(tags, '*', {
+        includePrerelease: false,
+    });
+
+    core.info(`current git tag in branch ${currentTagInBranch}`);
+    core.info(`latest git tag (including pre-releases): ${latestTagWithPreReleases}`);
+    core.info(`latest git tag (excluding pre-releases): ${latestTagWithoutPreReleases}`);
+
+    // calculate the SEMVER bump (major, minor, patch, premajor, preminor, prepatch)
+    // calculation uses the labels on PRs merged into current branch since the last release was cut
+    let semverVersionBump = await shelljs
+    .exec(
+        `./node_modules/.bin/auto version --from ${
+            releaseType === 'full-release'
+            ? latestTagWithoutPreReleases
+            : latestTagWithPreReleases
+        }`,
+        {
+            silent: true,
+        }
+    )
+    .stdout.trim();
+
+    core.info('original SEMVER bump calculated: ', semverVersionBump);
+
+    // manually strip out the 'pre' prefix (preminor, prepatch, premajor) if running a full release
+    if (releaseType === 'full-release') {
+        semverVersionBump = semverVersionBump.replace('pre', '');
+    }
+
+    core.info(`adjusted semverVersionBump after factoring in the type of release: ${semverVersionBump}`);
+
+    const nextVersion = await semver.inc(
+        latestTagWithPreReleases,
+        releaseType === 'pre-release' && semver.prerelease(latestTagWithPreReleases) !== null
+            ? 'prerelease'
+            : semverVersionBump,
+        releaseType === 'pre-release' ? 'rc' : undefined
+    );
+
+    // ^ calculate the next version, factoring in the current version, SEMVER bump, and release type
+    core.info('computed next version', nextVersion, releaseType);
+
+    if (!semver.valid(nextVersion) || nextVersion === null) {
+        throw Error(
+            `The calculated next version of the code, ${nextVersion}, is not valid. Existing early.`
+        );
+    }
+
+    // exit early if the version about to be released already exists
+    if (tags.length && tags.includes(`v${nextVersion}`)) {
+        throw Error(
+        `The next version we want to release, v${nextVersion}, appears to already exist on Github!`
+        );
+    }
+
+    console.log('---');
+
+    // is the version we're about to release OLDER than the latest release?
+    if (
+        tags.length &&
+        latestTagWithoutPreReleases !== null &&
+        !semver.gt(nextVersion, latestTagWithoutPreReleases.replace('v', ''))
+    ) {
+        throw Error(
+        `The next version slated to be released, v${nextVersion} is older than the latest stable release, ${latestTagWithoutPreReleases}`
+        );
     }
 
     if (dryRun === "true") {
         core.info("dry run, don't perform tagging");
         return;
     }
-    core.info(sha);
-    core.info(`currentTagInBranch: ${currentTagInBranch}`);
-    // const newTag = `${ withV }${ nextVersion }`;
+    const autoRelease = await shelljs.exec(
+        `npx auto release --from ${latestTagWithoutPreReleases} --use-version v${nextVersion}`
+    );
 
-    // core.info(`really add tag ${ customTag ? customTag : newTag }`);
-
-    // const ref = `refs/tags/${ customTag ? customTag : newTag }`;
-
-    // await octokit.rest.git.createRef({
-    //     owner,
-    //     repo,
-    //     ref,
-    //     sha
-    // });
+    if(autoRelease.ok) {
+        core.info(autoRelease.stdout.trim());
+    }else {
+        throw new Error(autoRelease.stderr);
+    }
+    
 }
 
 action()
