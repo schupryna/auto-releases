c6f76def1e8a0b45519b6ac6d42b65564ce8facc
Shabab Qaisar <sqaisar@pypestream.com>
2023-08-31 16:19:51 +0500

    Using slackformat for readme release text

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,7 @@
 /*eslint no-undef: "off"*/
 /*eslint no-trailing-spaces: "off"*/
 /*eslint no-unused-vars: "off"*/
+/*eslint no-useless-escape: "off"*/
 
 const fs = require('fs');
   
@@ -66,4 +67,89 @@ async function writeFile(fileName, content) {
     await fs.writeFileSync(fileName, JSON.stringify(content));
 }
 
-module.exports = { validateInputs, generateAutoRc, writeFile };
+const fixLinks = (str) => {
+    return str.replace(/\[([^\]]+)\]\(([^\)]+)\):?/g, '<$2|$1>');
+};
+
+const getSlackPayload = (messagePayload, owner, repo, tag) => {
+    const sectionBlocks = messagePayload.map(({title, listItems}) => {
+        return [
+            {
+                type: "section",
+                text: {
+                    type: "plain_text",
+                    text: title,
+                }
+            },
+            ...listItems.map((listItem) => {
+                return {
+                    type: "section",
+                    text: {
+                        type: "mrkdwn",
+                        text: listItem
+                    },
+                };
+            }),
+        ];
+    });
+    
+    const payload =  {
+        "attachments": [
+            {
+                "blocks": [
+                    {
+                        type: "section",
+                        text: {
+                            type: "mrkdwn",
+                            text: `<!here> - *:rocket: New release* has been published for *${owner}/${repo}* <https://github.com/${owner}/${repo}/releases/tag/${tag}|Release ${tag}>`
+                        },
+                    },
+                    {
+                        type: "divider"
+                    },
+                ]
+            }
+        ]
+    };
+
+    sectionBlocks.forEach(i =>{
+        i.forEach(k => {
+            payload.attachments[0].blocks.push(k);
+        });
+    });
+    return payload;
+};
+
+const formatSlackMessage = (content, owner, repo, tag) => {
+    const sections = content.split(/\n#+ /);
+
+    // Remove the empty string at the beginning of the array, if it exists
+    if (sections[0].trim() === '') {
+        sections.shift();
+    }
+    
+    // Create an array to hold the section objects
+    let sectionObjects = [];
+
+    sections.forEach((section) => {
+        const firstNewLine = section.indexOf('\n');
+        let title = firstNewLine === -1 ? section : section.substring(0, firstNewLine).trim();
+        let body = firstNewLine === -1 ? '' : section.substring(firstNewLine).trim();
+        
+        // Remove any remaining '#' from the title
+        title = title.replace(/#/g, '').trim();
+        
+        // Convert list items starting with "- " to an array
+        const listItems = body.split('\n')
+                                .filter(line => line.startsWith('- '))
+                                .map(line => line.substring(2))
+                                .map(fixLinks);
+        
+        // Push each section object to the array
+        sectionObjects.push({ title, listItems });
+    });
+
+    return getSlackPayload(sectionObjects, owner, repo, tag);
+};
+
+module.exports = { validateInputs, generateAutoRc, writeFile, formatSlackMessage };
